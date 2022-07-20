4d07fe28ecad4ebb032aaedd8afbef295bb97ec8
Marcello Santoro <marcelloaltermundi@gmail.com>
2022-07-20 09:48:41 -0300

    adiciona primeiros  exercícios de javascript

---
+++ Fundamentos/Bloco04-Introducao_JS_e_Logica_de_programacao/scripts.js
@@ -0,0 +1,56 @@
+//exercício 1
+const a;
+const b;
+
+let adicao = a + b;
+let subtracao = a - b;
+let multiplicacao = a * b;
+let divisao = a / b;
+let modulo = a % b;
+
+//exercício 2
+if (a > b) {
+    return a;
+} else {
+    return b;
+}
+
+
+//exercício 3
+const num = x;
+const num2 = y;
+const num3 = z;
+
+if (num > num2 && num > num3) {
+    return num
+} else if (num2 > num && num2 > num3) {
+    return num2
+} else {
+    return num3
+}
+
+switch(num, num2, num3){
+    case num > num2:
+    case num > num3:
+        return num;
+        break;
+    case num2 > num:
+    case num2 > num3:
+    return num2;
+        break;
+    case num3 > num:
+    case num3 > num2:
+        return num3;
+        break;
+
+}
+
+//exercício 5
+function verificaTriangulo (t1, t2, t3) {
+    if(t1 + t2 + t3 === 180){
+        return true
+    } else {
+        return false
+    }
+}
+
