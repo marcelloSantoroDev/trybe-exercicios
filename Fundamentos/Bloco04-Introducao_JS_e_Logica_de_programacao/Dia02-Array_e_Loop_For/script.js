6b6971ea0df91277e1c58f8fb574a911daf2325d
Marcello Santoro <marcelloaltermundi@gmail.com>
2022-07-21 16:22:33 -0300

    adiciona exercícios dia array loop

---
+++ Fundamentos/Bloco04-Introducao_JS_e_Logica_de_programacao/Dia02-Array_e_Loop_For/script.js
@@ -0,0 +1,60 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+//exercício 1
+
+//for (let i = 0 ; i < numbers.length ; i++){
+    //console.log(numbers[i]);
+//}
+
+//exercício 2
+
+//let result = 0;
+//for (let i = 0 ; i < numbers.length ; i++) {
+//  result += numbers[i]  
+//}
+
+//console.log(result);
+
+//exercício 3
+
+//let result = 0;
+//let mediaAritmetica;
+//for (let i = 0 ; i < numbers.length ; i++){
+//    result = result + numbers[i]
+//}
+
+//mediaAritmetica = result / numbers.length
+
+//console.log(mediaAritmetica);
+
+//exercício 4
+
+//if(mediaAritmetica > 20) {
+//    console.log('valor maior que 20');
+//} else {
+//    console.log('valor menor ou igual a 20');
+//}
+
+//exercício 5
+
+//let maiorValor = 0;
+
+//for (let i = 0 ; i < numbers.length ; i++){
+//    if (numbers[i] > maiorValor){
+//        maiorValor = numbers[i]
+//}
+
+//}
+//console.log(maiorValor);
+
+//exercício 6
+let valoresImpar = [];
+for (let i = 0 ; i < numbers.length ; i++){
+    if(numbers[i] % 2 != 1){
+        valoresImpar.push(numbers[i]);
+    } else {
+        console.log('nenhum valor ímpar encontrado');
+    }
+}
+
+
