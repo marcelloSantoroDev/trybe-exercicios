0566e0a147abe655d5cfe522500a40f52663154e
Marcello Santoro <marcelloaltermundi@gmail.com>
2022-07-22 17:19:59 -0300

    adiciona exercícios do dia 3

---
+++ Fundamentos/Bloco04-Introducao_JS_e_Logica_de_programacao/Dia3-Lógica_Programacao_e_Algoritmo/script.js
@@ -0,0 +1,32 @@
+// let fruits = [3, 4, 10, 1, 12];
+// let resultado = 0;
+
+// for(let i = 0 ; i < fruits.length ; i+= 1){
+//     resultado = resultado + fruits[i];
+// }
+// if(resultado > 15){
+//     console.log(resultado);
+// } else {
+//     console.log('valor menor que 16');
+// }
+
+
+// let resultado = 10;
+
+// for(let i = 9 ; i > 0 ; i--) {
+//     resultado *= i
+// }
+
+// console.log(resultado);
+
+// let word = 'tryber';
+// let invertedWord = '';
+
+
+for(let j = word.length - 1 ; j >= 0 ; j -= 1){
+   invertedWord += word[j] 
+}
+
+console.log(invertedWord);
+
+
