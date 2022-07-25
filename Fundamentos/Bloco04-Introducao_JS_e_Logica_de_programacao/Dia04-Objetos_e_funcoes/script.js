3014125fd73734f367c6893c8c4f011de5e30507
Marcello Santoro <marcelloaltermundi@gmail.com>
2022-07-25 17:56:18 -0300

    adiciona exercícios 1 e 2

--- Fundamentos/Bloco04-Introducao_JS_e_Logica_de_programacao/Dia04-Objetos_e_funcoes/script.js
+++ Fundamentos/Bloco04-Introducao_JS_e_Logica_de_programacao/Dia04-Objetos_e_funcoes/script.js
@@ -16,4 +16,89 @@
 
 // console.log('A jogadora possui', player.medals.golden , 'medalhas de outro e', player.medals.silver, 'medalhas de prata');
 
+//Para fixar for/in for/of
+
+// let names = {
+//     person1: 'João',
+//     person2: 'Maria',
+//     person3: 'Jorge',
+//   };
+
+//   for(let firstName in names){
+//     console.log('Olá,', names[firstName]);
+//   }
+
+// let car = {
+//     model: 'A3 Sedan',
+//     manufacturer: 'Audi',
+//     year: 2020
+//   };
+
+//   for(let key in car){
+//     console.log(key, car[key]);
+//   }
+
+//Exercícios Objetos e Funções
+
+
+
+// let str = 'ararar'
+
+//     function verificaPalindromo(str){
+
+//     let strInvertida = [];
+//     let ehPalindromo;
+
+//         for(let i = str.length - 1; i >= 0; i -=1){
+//             strInvertida.push(str[i]);            
+//     }
+
+//     let transformString = strInvertida.join('');
+
+//     if(str == transformString){
+//         ehPalindromo = true
+//     } else {
+//         ehPalindromo = false
+//     }
+
+// return ehPalindromo;
+
+// }
+
+//     console.log(verificaPalindromo('arara'));
+
+//Exercício 2
+
+function arrayIndex(arr) {
+
+    let biggestNumber = 0;
+    for (let i = 0; i < arr.length; i+=1) {
+        if (arr[i] > biggestNumber) {
+            biggestNumber = i
+        }
+
+    }
+
+    return biggestNumber;
+
+}
+
+console.log(arrayIndex([20, 3, 6, 10, 1]));
+
+// function arrayIndex(arr) {
+    // let arr = [-1, 6, 0, 7, 10, 3]
+    // let smallestNumber = arr[0];
+    // for (let i = 0; i < arr.length; i += 1) {
+    //     console.log(i);
+    //     if (arr[i] < smallestNumber) {
+    //         console.log(i);
+    //     }
+
+    // }
+
+    // console.log(smallestNumber);
+
+//}
+
+// console.log(arrayIndex([1, 6, 7, 10, 3]));
 
