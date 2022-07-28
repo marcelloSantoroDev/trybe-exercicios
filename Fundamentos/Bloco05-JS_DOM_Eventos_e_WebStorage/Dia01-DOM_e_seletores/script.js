ff05cef5d5a2154f452566e44dd0c896bc79a902
Marcello Santoro <marcelloaltermundi@gmail.com>
2022-07-28 14:03:05 -0300

    adidiona algumas resoluções

---
+++ Fundamentos/Bloco05-JS_DOM_Eventos_e_WebStorage/Dia01-DOM_e_seletores/script.js
@@ -0,0 +1,11 @@
+let text2 = document.querySelector(".emergency-tasks h3");
+text2.style.backgroundColor = "purple";
+
+let text4 = document.querySelector("#footer-container");
+text4.style.color = "black";
+
+let bkgrdColor1 = document.querySelector("#header-container");
+bkgrdColor1.style.backgroundColor = "green";
+
+let a = document.querySelector(".no-urgents");
+a.style.backgroundColor = "black";
